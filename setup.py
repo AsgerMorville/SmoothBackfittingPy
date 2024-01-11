9f607b696160a5a6ee033cc86dc81c8d0362d3c2
Asger Morville <asgermorville@gmail.com>
2024-01-11 14:03:42 +0900

    deleted files

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
 
 #
@@ -18,17 +17,19 @@ from Cython.Build import cythonize
 USE_CYTHON = True
 
 if USE_CYTHON:
+    from Cython.Build import cythonize
     sourcefiles = ['SmoothBackfittingPy.pyx']
     extensions = [Extension(name="SmoothBackfittingPy", sources=sourcefiles, extra_compile_args=["-std=c++20"],extra_link_args=["-std=c++20"])]
     setup(
         ext_modules=cythonize(extensions,compiler_directives={'language_level' : "3"}),
-        include_dirs = ['./include', './extern'])
+        include_dirs = ['SmoothBackfittingCpp/include', 'SmoothBackfittingCpp/extern']
+        )
 else:
     sourcefiles = ['SmoothBackfittingPy.cpp']
     setup(
         ext_modules = [Extension(name="SmoothBackfittingPy", sources=sourcefiles)],
-        include_dirs = ['.']
-    )
+        include_dirs = ['SmoothBackfittingCpp/include', 'SmoothBackfittingCpp/extern']
+        )
 
 
 
