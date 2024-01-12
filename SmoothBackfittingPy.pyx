5b375eccb84e4a2a0d63bbc07ea379ca34582f5a
Asger Morville <asgermorville@gmail.com>
2024-01-12 14:21:27 +0900

    partially linear add

--- SmoothBackfittingPy.pyx
+++ SmoothBackfittingPy.pyx
@@ -13,3 +13,12 @@ def SBF_CPP(double[:] Y, double[:,:] X, double[:] output, int n, int d):
     #sumfunc.sumfunction1(&arr1[0,0],&arr2[0,0], &narr_view[0,0], dim1)
     sbf.sbfWrapper(&Y[0],&X[0,0], &output[0], n, d)
     return
+
+def PL_SBF_CPP(double[:] Y, double[:,:] X, double[:,;] Z,  double[:] output, int n, int d_x, int d_z):
+    #output_mat = np.zeros((dim1,dim2))
+    #output_mat = output_mat.astype(float)
+    #narr_view = cython.declare(cython.float[:, :, :], output_mat)
+    # Directly pass the pointer to the data of the NumPy array
+    #sumfunc.sumfunction1(&arr1[0,0],&arr2[0,0], &narr_view[0,0], dim1)
+    sbf.sbfWrapper(&Y[0],&X[0,0], &output[0], n, d)
+    return
--- sbf.pxd
+++ sbf.pxd
@@ -4,3 +4,5 @@
 cdef extern from "smooth_backfitting_core.h":
     void sbfWrapper(double* yPtr, double* xPtr, double* outputPtr, int n, int d)
 
+cdef extern from "partially_linear_SBF.h":
+    void plSBFWrapper(double* yPtr, double* xPtr, double* zPtr, double* outputPtr, int n, int d_x, int d_z)
