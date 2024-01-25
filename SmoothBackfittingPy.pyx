d8f74f4fe706ba6eaa1243aa0d37b77e2b7d9ab0
Asger Morville <asgermorville@gmail.com>
2024-01-26 08:51:19 +0900

    t1

--- SmoothBackfittingPy.pyx
+++ SmoothBackfittingPy.pyx
@@ -14,6 +14,13 @@ def SBF_CPP(double[:] Y, double[:,:] X, double[:] output, int n, int d):
     sbf.sbfWrapper(&Y[0],&X[0,0], &output[0], n, d)
     return
 
+def SBF_test(double[:] Y, double[:,:] X):
+    n, d = X.shape
+    output_vec = np.zeros(n)
+    SBF_CPP(Y, X, output_vec, n, d)
+    return output_vec
+
 def PL_SBF_CPP(double[:] Y, double[:,:] X, double[:,:] Z,  double[:] output, int n, int d_x, int d_z):
     sbf.plSBFWrapper(&Y[0],&X[0,0], &Z[0,0], &output[0], n, d_x, d_z)
     return
+
