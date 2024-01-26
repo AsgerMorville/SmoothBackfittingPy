6f26251743e918b72bdd61e04a1ddb1db1289e03
Asger Morville <asgermorville@gmail.com>
2024-01-26 13:32:02 +0900

    setup.py

--- SmoothBackfittingPy.pyx
+++ SmoothBackfittingPy.pyx
@@ -6,21 +6,16 @@ import numpy as np
 import cython
 
 def SBF_CPP(double[:] Y, double[:,:] X, double[:] output, int n, int d):
-    #output_mat = np.zeros((dim1,dim2))
-    #output_mat = output_mat.astype(float)
-    #narr_view = cython.declare(cython.float[:, :, :], output_mat)
-    # Directly pass the pointer to the data of the NumPy array
-    #sumfunc.sumfunction1(&arr1[0,0],&arr2[0,0], &narr_view[0,0], dim1)
     sbf.sbfWrapper(&Y[0],&X[0,0], &output[0], n, d)
     return
-
-def SBF_test(Y, X):
-    n, d = X.shape
-    output_vec = np.zeros(n)
-    SBF_CPP(Y, X, output_vec, n, d)
-    return output_vec
 
 def PL_SBF_CPP(double[:] Y, double[:,:] X, double[:,:] Z,  double[:] output, int n, int d_x, int d_z):
     sbf.plSBFWrapper(&Y[0],&X[0,0], &Z[0,0], &output[0], n, d_x, d_z)
     return
+
+def SBF(Y, X):
+    n, d = X.shape
+    output_vec = np.zeros(n)
+    SBF_CPP(Y, X, output_vec, n, d)
+    return output_vec
 
--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-
-#
-#sourcefiles = ['SmoothBackfittingPy.pyx']
-
-#extensions = [Extension(name="SmoothBackfittingPy", sources=sourcefiles, extra_compile_args=["-std=c++20"],extra_link_args=["-std=c++20"])]
-
-#setup(
-#    ext_modules=cythonize(extensions,compiler_directives={'language_level' : "3"}),
-#    include_dirs = ['./include', './extern']    
-#)
-
-
-
 USE_CYTHON = True
 
 if USE_CYTHON:
@@ -30,11 +17,3 @@ else:
         ext_modules = [Extension(name="SmoothBackfittingPy", sources=sourcefiles)],
         include_dirs = ['SmoothBackfittingCpp/include', 'SmoothBackfittingCpp/extern']
         )
-
-
-
-#setup(
-#    name='TestName',
-#    ext_modules=cythonize("sumCython.pyx"),
-#    include_dirs=[numpy.get_include()]
-#)
